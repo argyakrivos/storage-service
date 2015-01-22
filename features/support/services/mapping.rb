2df220b36e5a3989c43c715ed60acb5334c85170
Mohammed Abdulrazeg <mohammed@blinkbox.com>
2015-01-22 17:40:06 +0000

    Updated tests to use generic v2 application headers and Swagger too

--- features/support/services/mapping.rb
+++ features/support/services/mapping.rb
@@ -1,6 +1,6 @@
 module KnowsAboutMappingApi
   def get_mapping_file
-    http_get :storage_service, "mappings", "Accept" => "application/vnd.blinkbox.books.ingestion.storageservice.v2+json"
+    http_get :storage_service, "mappings", "Accept" => "application/vnd.blinkbox.books.v2+json"
     @response_data = parse_last_api_response
   end
 end
